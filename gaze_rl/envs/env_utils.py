971ec23ee7d804b0ede6cc0bc1544d9ce4d0bb07
Dhanushvarma <pdhanu2001@gmail.com>
2025-03-20 13:09:14 -0700

    started making changes for MuJoCo envs

--- gaze_rl/envs/env_utils.py
+++ gaze_rl/envs/env_utils.py
@@ -1,12 +1,17 @@
 import gymnasium as gym
 
 from gaze_rl.utils.logger import log
+from gaze_rl.envs.env_wrappers import ImageObservationWrapper, FrameStackWrapper
 
 
 def make_envs(cfg, training: bool = True):
     if cfg.env.name == "mujoco":
-        raise NotImplementedError
-        # TODO(dhanush): Implement Env Creation for MuJoCo tasks
 
+        # Create environment with wrappers as needed
+        env = gym.make(cfg.env.env_id)
+
+        # TODO: add wrapper as needed here
+
+        return env
     else:
         raise NotImplementedError
